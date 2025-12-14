e7232682677b377e3eba5db11aadaef839a7a2b7
Mayank Pandey <121632123+Mayank-Pandey1@users.noreply.github.com>
2025-12-14 20:15:47 +0530

    Create Codeforces - 380c Sereja and brackets.cpp

---
+++ SegmentTrees/Codeforces - 380c Sereja and brackets.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class SGTree {
+    public:
+    vector<int> seg;
+
+    SGTree(int n) {
+        seg.resize(4*n+1);
+    }
+    
+};
+
+void solve() {
+    
+}
+int main() {
+    solve();
+    return 0;
+}
